675baca727c6d5eac0f6cdd1d3a5c947d99d930f
julienV99 <47658316+julienV99@users.noreply.github.com>
2020-10-12 14:44:34 +0200

    q1,2,3

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -8,10 +8,12 @@ const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 const API_URL_ICON = "http://openweathermap.org/img/wn/";
 
 
+
+
 class API_WEATHER{
   constructor(city){
     // Si la ville n'est pas définit alors la ville par défault est Paris
-    if(city === undefined){
+    if(city === ""){
       city = "paris";
     }
     this.city = city;
